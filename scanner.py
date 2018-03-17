a371b3a25a4587bf20944316a6c61d53e13df48e
GIANNIS-AGGELIS <GIANNIS-AGGELIS@users.noreply.github.com>
2018-03-17 22:56:18 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,66 +1,53 @@
-
 def getchar(words,pos):
 	""" returns char at pos of words, or None if out of bounds """
-
 	if pos<0 or pos>=len(words): return None
-
 	return words[pos]
-	
 
 def scan(text,transition_table,accept_states):
 	""" Scans `text` while transitions exist in 'transition_table'.
 	After that, if in a state belonging to `accept_states`,
 	returns the corresponding token, else ERROR_TOKEN.
 	"""
-	
 	# initial state
 	pos = 0
 	state = 'q0'
 	
 	while True:
-		
 		c = getchar(text,pos)	# get next char
-		
 		if state in transition_table and c in transition_table[state]:
-		
 			state = transition_table[state][c]	# set new state
 			pos += 1	# advance to next char
-			
 		else:	# no transition found
-
 			# check if current state is accepting
 			if state in accept_states:
 				return accept_states[state],pos
-
 			# current state is not accepting
 			return 'ERROR_TOKEN',pos
 			
 	
 # the transition table, as a dictionary
-
-# Αντικαταστήστε με το δικό σας λεξικό μεταβάσεων...
-td = { 'q0':{ 't':'q1','l':'q2' },
-       'q1':{ 'e':'q3' },
-       'q2':{ 'o':'q8' },
-       'q3':{ 's':'q4','r':'q6' },
-       'q4':{ 't':'q5' },
-       'q6':{ 'm':'q7' },
-       'q8':{ 'n':'q9' },
-       'q9':{ 'g':'q10'}
+td = { 
+        'q0':{'0':'q1','1':'q1','2':'q2','3':'q3','4':'q3','5':'q3','6':'q3','7':'q3','8':'q3','9':'q3'},
+        'q1':{'0':'q4','1':'q4','2':'q4','3':'q4','4':'q4','5':'q4','6':'q4','7':'q4','8':'q4','9':'q4','.':'q5',':':'q5'},
+        'q2':{'0':'q4','1':'q4','2':'q4','3':'q4','4':'q8','.':'q5',':':'q5'},
+        'q3':{'.':'q5',':':'q5'},
+        'q4':{'.':'q5',':':'q5'},
+        'q5':{'0':'q6','1':'q6','2':'q6','3':'q6','4':'q6','5':'q6'},
+        'q6':{'0':'q7','1':'q7','2':'q7','3':'q7','4':'q7','5':'q7','6':'q7','7':'q7','8':'q7','9':'q7'},
+        'q8':{'.':'q9',':':'q9'},
+        'q9':{'0':'q10'},
+        'q10':{'0':'q11'}
+        
      } 
 
 # the dictionary of accepting states and their
 # corresponding token
-
-# Αντικαταστήστε με το δικό σας λεξικό καταστάσεων αποδοχής...
-ad = { 'q5':'TEST_TOKEN',
-       'q7':'TERM_TOKEN',
-       'q10':'LONG_TOKEN'
+ad = { 'q7':'TIME_TOKEN',
+       'q11':'TIME_TOKEN'
      }
 
-
 # get a string from input
-text = input('give some input>')
+text = input('give some input> ')
 
 # scan text until no more input
 while text:	# that is, while len(text)>0
@@ -69,6 +56,7 @@ while text:	# that is, while len(text)>0
 	token,position = scan(text,td,ad)
 	
 	if token=='ERROR_TOKEN':
+		print(token)
 		print('unrecognized input at pos',position+1,'of',text)
 		break
 	
@@ -76,4 +64,3 @@ while text:	# that is, while len(text)>0
 	
 	# remaining text for next scan
 	text = text[position:]
-	
